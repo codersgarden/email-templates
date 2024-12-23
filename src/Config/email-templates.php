e6020276213263e93fff17951206d1ed3de28005
Dev Codersgarden <dev@codersgarden.de>
2024-12-23 17:14:23 +0530

    variable added in config file

--- src/Config/email-templates.php
+++ src/Config/email-templates.php
@@ -1,11 +1,7 @@
 <?php
 
 return [
-    // Default email sender
     'default_sender' => env('MAIL_FROM_ADDRESS', 'no-reply@example.com'),
-
-    // Supported locales
     'supported_locales' => ['en', 'fr', 'de'],
-
-    // Other package-specific settings
+    'placeholder_data_types' => ['string', 'array', 'object', 'int', 'float', 'boolean'],
 ];
